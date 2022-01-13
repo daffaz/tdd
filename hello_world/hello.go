ca3a4bd4d41c11a9ab743785a59fbd8c2e4437e8
Daffa Z <67418594+daffaz@users.noreply.github.com>
2022-01-14 01:05:12 +0700

    ref

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -4,15 +4,31 @@ import "fmt"
 
 const (
 	englishHelloPrefix = "Hello"
+	spanishHelloPrefix = "Hola"
+	franceHelloPrefix  = "Bonjour"
 )
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if len(name) <= 0 {
-		return fmt.Sprintf("%s, World", englishHelloPrefix)
+		name = "World"
 	}
-	return fmt.Sprintf("%s, %s", englishHelloPrefix, name)
+
+	return fmt.Sprintf("%s, %s", greetingPrefix(language), name)
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "es":
+		prefix = spanishHelloPrefix
+	case "fr":
+		prefix = franceHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Wawan"))
+	fmt.Println(Hello("Wawan", ""))
 }
