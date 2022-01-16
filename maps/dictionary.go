e6a71cbce5bb8def74f6f5635f1bbc25059bf164
Daffa Z <67418594+daffaz@users.noreply.github.com>
2022-01-17 02:02:24 +0700

    add method in dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"errors"
+)
+
+var ErrorNotFound = errors.New("not found")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(target string) string {
-	return d[target]
+func (d Dictionary) Search(target string) (string, error) {
+	val, ok := d[target]
+	if ok {
+		return val, nil
+	}
+	return "", ErrorNotFound
+}
+
+func (d Dictionary) Add(key, value string) {
+	d[key] = value
 }
